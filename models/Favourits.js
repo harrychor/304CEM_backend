89aafb75065f04c011a895bbec885899a432464a
harrychor <58283630+harrychor@users.noreply.github.com>
2020-05-23 19:08:14 +0800

    fav update

--- models/Favourits.js
+++ models/Favourits.js
@@ -18,7 +18,7 @@ const FavouritesSchema = new mongoose.Schema({
         trim: true
     },
     imdbID: {
-        type: Number,
+        type: String,
         required: true,
         trim: true
     },
